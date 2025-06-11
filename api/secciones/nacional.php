69d231513c034f88dc7729b8bb9f662326256950
Jasenovec <151959647+Jasenovec@users.noreply.github.com>
2025-06-11 10:16:46 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -6,6 +6,6 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 $nacional = [
 "titulo" => "Un explosivo detonó en obra de la Estación Central de la Línea 2 del Metro de Lima",
 "autor" => "Rodrigo Montano",
-"resumen" => "Un artefacto explosivo fue detonado esta madrugada en las inmediaciones de la obra de construcción de la Estación Central de la Línea 2 del Metro de Lima.",
+"resumen" => "Un artefacto explosivo fue detonado esta madrugada en Estación Central de la Línea 2 del Metro de Lima.",
 ];
 ?>
