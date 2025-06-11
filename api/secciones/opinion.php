899cce5f14857fe98314743690972bc92caad8c6
Jasenovec <151959647+Jasenovec@users.noreply.github.com>
2025-06-11 10:21:24 -0500

    Update opinion.php

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -5,7 +5,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 
 $opinion = [
 "titulo" => "Preocupante deterioro fiscal, por Hugo Perea",
-"autor" => "odrigo Montano",
+"autor" => "Rodrigo Montano",
 "resumen" => "La deuda pública neta (de los activos del gobierno) ha mostrado un deterioro más marcado: se encuentra en 23% en 2024.",
 ];
 ?>
