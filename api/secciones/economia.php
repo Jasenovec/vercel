829353017512712b32f88a5e8b18f0b6b5e0b918
Jasenovec <151959647+Jasenovec@users.noreply.github.com>
2025-06-11 10:19:38 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "Dictamen del retiro AFP se postergó: Comisión de Economía esperará al reglamento de la reforma de pensiones.",
+"titulo" => "Retiro AFP se postergó: Comisión de Economía esperará al reglamento de la reforma de pensiones.",
 "autor" => "Rodrigo Montano",
-"resumen" => "Retiro AFP 2025 podría demorar. El único punto de la agenda de la sesión es un informe de los proyectos de ley que plantean modificaciones a la reforma previsional.",
+"resumen" => "El único punto de la agenda de la sesión es un informe de los proyectos de ley que plantean modificaciones a la reforma previsional.",
 ];
 ?>
