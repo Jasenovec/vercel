93bc7dfc4e636c94323bc71edee7a85957b2195f
Jasenovec <151959647+Jasenovec@users.noreply.github.com>
2025-06-11 10:14:37 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -6,6 +6,6 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 $internacional = [
 "titulo" => "Guerra Ucrania - Rusia, últimas noticias | Un mortífero bombardeo ruso en dos distritos residenciales de Jarkiv deja tres muertos y 64 heridos, entre ellos niños",
 "autor" => "Rodrigo Montano",
-"resumen" => "Ucrania anuncia haber recuperado 1.212 cuerpos de soldados muertos en el frente con Rusia y mañana se intercambiarán los prisioneros gravemente heridos.",
+"resumen" => "Ucrania anuncia haber recuperado 1.212 cuerpos de soldados muertos en el frente con Rusia.",
 ];
 ?>
